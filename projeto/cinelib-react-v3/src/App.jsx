a48dbd3e588648f73e5fc53f039daa984fb9d7ab
lucas-lfm <mendes.lucas.lfm@gmail.com>
2025-01-14 17:06:29 -0300

    atualização do roteiro de prática 03 - projeto final

--- projeto/cinelib-react-v3/src/App.jsx
+++ projeto/cinelib-react-v3/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
 
@@ -11,7 +12,7 @@ const App = () => {
 
       <main>
         <div className="container">
-
+          <Outlet />
         </div>
       </main>
 
--- projeto/cinelib-react-v3/src/main.jsx
+++ projeto/cinelib-react-v3/src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
+import Home from './pages/Home/Home.jsx'
+import Search from './pages/Search/Search.jsx'
+
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/search",
+        element: <Search />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
