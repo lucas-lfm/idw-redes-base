bbc260f3500b3e870e2574f8641fbde60c02169c
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2025-01-18 00:07:19 -0300

    roteiro de prática 04

---
+++ projeto/cinelib-react-v4/src/components/NavBar.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./NavBar.css";
+import searchIcon from "./../assets/search-icon.svg";
+import cinelibLogo from "/cine-lib.svg";
+
+const NavBar = () => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const search = (e) => {
+    e.preventDefault();
+
+    navigate(`/search?q=${query}`);
+
+    setQuery("");
+  };
+
+  return (
+    <header>
+      <nav id='navbar'>
+        <div id='brand'>
+          <a href='/'>
+            <img src={cinelibLogo} alt='Logo CineLib' />
+            <h2>CineLib</h2>
+          </a>
+        </div>
+        <form onSubmit={search} id='search-form'>
+          <input
+            type='text'
+            placeholder='Busque um filme...'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type='submit'>
+            <img src={searchIcon} alt='Ícone de pesquisa' />
+          </button>
+        </form>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
